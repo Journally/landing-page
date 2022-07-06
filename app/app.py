40eea0f515a926057b8e4678bf017c25fb1fbf72
Natalie Volk <natalievolk@rogers.com>
2022-07-06 00:59:56 -0400

    create UI

--- app/app.py
+++ app/app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def welcome():
     return render_template("index.html")
+
+@app.route('/thank-you')
+def thank_you():
+    return render_template("thank-you.html")
