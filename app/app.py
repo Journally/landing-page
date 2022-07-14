8565eeb98d304b1d430ec3cb301bdca999cd97ab
Natalie Volk <natalievolk@rogers.com>
2022-07-13 22:34:56 -0400

    add prod and dev environments

--- app/app.py
+++ app/app.py
@@ -8,10 +8,11 @@ load_dotenv()
 
 app = Flask(__name__)
 
-client = MongoClient(os.getenv('MONGODB_URI'), connect=False, tlsCAFile=certifi.where())
-# client = MongoClient(f"mongodb+srv://admin:{os.getenv('MONGODB_PASSWORD')}@journally.ywk9p1u.mongodb.net/?retryWrites=true&w=majority", connect=False, tlsAllowInvalidCertificates=True)
-db = client.landing_page
-prereg = db.prereg
+if os.getenv('ENV') == 'prod':
+    client = MongoClient(os.getenv('MONGODB_URI'), connect=False, tlsCAFile=certifi.where())
+    # client = MongoClient(f"mongodb+srv://admin:{os.getenv('MONGODB_PASSWORD')}@journally.ywk9p1u.mongodb.net/?retryWrites=true&w=majority", connect=False, tlsAllowInvalidCertificates=True)
+    db = client.landing_page
+    prereg = db.prereg
 
 @app.route('/')
 def welcome():
@@ -23,6 +24,7 @@ def thank_you():
     email = request.form.get("exampleInputEmail")
     print(name, email)
 
-    prereg.insert_one({'name': name, 'email': email})
+    if os.getenv('ENV') == 'prod':
+        prereg.insert_one({'name': name, 'email': email})
 
     return render_template("thank-you.html")
